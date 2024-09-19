fbb0937896648feb497eee2039a4c151c10a00b7
Richard Crowley <r@rcrowley.org>
2024-09-18 22:22:39 -0700

    Deadlinks

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"slices"
+	"testing"
+)
+
+var (
+	ignored = []string{"../dead.html", "/dead.html", "/dead/", "dead/", "https://rcrowley.org/dead.html"}
+	verbose bool
+)
+
+func TestScan(t *testing.T) {
+	deadlinks, err := scan([]string{"test"}, []string{}, &verbose)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !slices.Equal(deadlinks, ignored) {
+		t.Fatal(deadlinks)
+	}
+}
+
+func TestScanIgnore(t *testing.T) {
+	deadlinks, err := scan([]string{"test"}, ignored, &verbose)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(deadlinks) != 0 {
+		t.Fatal(deadlinks)
+	}
+}
