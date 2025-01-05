5b53d1cd974a281f72a7eee33a6970b9401e6534
Richard Crowley <r@rcrowley.org>
2025-01-05 00:18:11 -0800

    Refactor to use files.AllHTML and nest test data under testdata/

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	"io/fs"
 	"log"
 	"net/http"
 	"net/url"
@@ -57,8 +56,9 @@ Synopsis: deadlinks scans one or more directories for <a>, <img>, <link rel="sty
 	} else {
 		docroots = flags.Args()
 	}
+	lists := must2(files.AllHTML(docroots, *exclude))
 
-	deadlinks := must2(scan(docroots, ignored, verbose)) // TODO use files.All
+	deadlinks := must2(scan(lists, ignored, verbose))
 	if *verbose {
 		fmt.Fprintf(os.Stderr, "\nfound %d dead links", len(deadlinks))
 		if len(deadlinks) > 0 {
@@ -94,24 +94,84 @@ func must2[T any](v T, err error) T {
 	return v
 }
 
-func scan(dirs, ignored []string, verbose *bool) (deadlinks []string, err error) {
+func scan(lists []files.List, ignored []string, verbose *bool) (deadlinks []string, err error) {
 	cache := make(map[string]bool)
-	for _, dir := range dirs {
-		if dir, err = filepath.Abs(dir); err != nil {
-			return
-		}
-		walk := walker(dir, cache, ignored, verbose)
-		var fi fs.FileInfo
-		if fi, err = os.Stat(dir); err != nil {
-			return
-		}
-		if fi.IsDir() {
-			err = fs.WalkDir(os.DirFS(dir), ".", walk)
-		} else {
-			err = walk(dir, fs.FileInfoToDirEntry(fi), nil)
-		}
-		if err != nil {
-			return
+	for _, list := range lists {
+		for _, path := range list.RelativePaths() {
+			if *verbose {
+				log.Printf("scanning %s\n", filepath.Join(list.Root(), path))
+			}
+			dir := filepath.Dir(path)
+
+			in := must2(html.ParseFile(filepath.Join(list.Root(), path)))
+			for _, out := range html.FindAll(in, html.Any(
+				html.Match(must2(html.ParseString(`<a>`))),
+				html.Match(must2(html.ParseString(`<img>`))),
+				html.Match(must2(html.ParseString(`<link rel="stylesheet">`))),
+				html.Match(must2(html.ParseString(`<script>`))),
+				html.Match(must2(html.ParseString(`<style>`))),
+			)) {
+				href := html.Attr(out, "href")
+				if href == "" {
+					href = html.Attr(out, "src") // different name but we treat it the same
+				}
+				if href == "" || href == "#" {
+					continue
+				}
+
+				if _, ok := cache[href]; ok {
+					continue
+				}
+
+				if i := sort.SearchStrings(ignored, href); i < len(ignored) && ignored[i] == href {
+					if *verbose {
+						log.Printf("ignoring %s", href)
+					}
+					continue
+				}
+
+				u, err := url.Parse(href)
+				if err != nil {
+					log.Print(err)
+					cache[href] = false
+				}
+
+				if u.Scheme == "http" || u.Scheme == "https" {
+					resp, err := http.Head(u.String())
+					cache[href] = err == nil && resp.StatusCode < http.StatusBadRequest
+
+				} else if u.Scheme == "mailto" {
+					cache[href] = true // TODO test this mailbox by actually connecting to the SMTP server
+
+				} else if u.Scheme == "tel" {
+					cache[href] = true // we're not going to try to verify phone numbers, come on
+
+				} else if u.Path != "" {
+					hrefPath := u.Path
+					if !strings.HasPrefix(u.Path, "/") {
+						hrefPath = filepath.Join(dir, u.Path)
+					}
+					if fi, err := os.Stat(filepath.Join(list.Root(), hrefPath)); err == nil && !fi.IsDir() {
+						cache[href] = true
+					} else if fi, err := os.Stat(filepath.Join(list.Root(), hrefPath, "index.html")); err == nil && !fi.IsDir() {
+						cache[href] = true
+					} else {
+						cache[href] = false
+					}
+
+				} else if fragment := u.EscapedFragment(); fragment != "" {
+					matcher := html.HasAttr("id", fragment)
+					if id, err := url.QueryUnescape(fragment); err == nil {
+						matcher = html.Any(matcher, html.HasAttr("id", id))
+					}
+					cache[href] = html.Find(in, matcher) != nil
+
+				} else {
+					cache[href] = false
+					log.Printf("unclear how to test %s", href)
+
+				}
+			}
 		}
 	}
 
@@ -122,89 +182,4 @@ func scan(dirs, ignored []string, verbose *bool) (deadlinks []string, err error)
 	}
 	sort.Strings(deadlinks)
 	return
-}
-
-func walker(dir string, cache map[string]bool, ignored []string, verbose *bool) func(string, fs.DirEntry, error) error {
-	return func(path string, d fs.DirEntry, err error) error {
-		must(err)
-
-		if !d.Type().IsRegular() || filepath.Ext(path) != ".html" {
-			return nil
-		}
-		if *verbose {
-			log.Printf("scanning %s\n", filepath.Join(dir, path))
-		}
-
-		in := must2(html.ParseFile(filepath.Join(dir, path)))
-		for _, out := range html.FindAll(in, html.Any(
-			html.Match(must2(html.ParseString(`<a>`))),
-			html.Match(must2(html.ParseString(`<img>`))),
-			html.Match(must2(html.ParseString(`<link rel="stylesheet">`))),
-			html.Match(must2(html.ParseString(`<script>`))),
-			html.Match(must2(html.ParseString(`<style>`))),
-		)) {
-			href := html.Attr(out, "href")
-			if href == "" {
-				href = html.Attr(out, "src") // different name but we treat it the same
-			}
-			if href == "" || href == "#" {
-				continue
-			}
-
-			if _, ok := cache[href]; ok {
-				continue
-			}
-
-			if i := sort.SearchStrings(ignored, href); i < len(ignored) && ignored[i] == href {
-				if *verbose {
-					log.Printf("ignoring %s", href)
-				}
-				continue
-			}
-
-			u, err := url.Parse(href)
-			if err != nil {
-				log.Print(err)
-				cache[href] = false
-			}
-
-			if u.Scheme == "http" || u.Scheme == "https" {
-				resp, err := http.Head(u.String())
-				cache[href] = err == nil && resp.StatusCode < http.StatusBadRequest
-
-			} else if u.Scheme == "mailto" {
-				cache[href] = true // TODO test this mailbox by actually connecting to the SMTP server
-
-			} else if u.Scheme == "tel" {
-				cache[href] = true // we're not going to try to verify phone numbers, come on
-
-			} else if u.Path != "" {
-				hrefPath := u.Path
-				if !strings.HasPrefix(u.Path, "/") {
-					hrefPath = filepath.Join(filepath.Dir(path), u.Path)
-				}
-				if fi, err := os.Stat(filepath.Join(dir, hrefPath)); err == nil && !fi.IsDir() {
-					cache[href] = true
-				} else if fi, err := os.Stat(filepath.Join(dir, hrefPath, "index.html")); err == nil && !fi.IsDir() {
-					cache[href] = true
-				} else {
-					cache[href] = false
-				}
-
-			} else if fragment := u.EscapedFragment(); fragment != "" {
-				matcher := html.HasAttr("id", fragment)
-				if id, err := url.QueryUnescape(fragment); err == nil {
-					matcher = html.Any(matcher, html.HasAttr("id", id))
-				}
-				cache[href] = html.Find(in, matcher) != nil
-
-			} else {
-				cache[href] = false
-				log.Printf("unclear how to test %s", href)
-
-			}
-		}
-
-		return nil
-	}
 }
